e55ceb7f6a1ab255b7ddeacd83a496af390bec67
apetrelli <antonio.petrelli@gmail.com>
2014-07-07 17:51:49 +0200

    Changed an interface.

--- gwt-integration-mvp/src/main/java/com/github/apetrelli/gwtintegration/mvp/client/ui/ListView.java
+++ gwt-integration-mvp/src/main/java/com/github/apetrelli/gwtintegration/mvp/client/ui/ListView.java
@@ -3,9 +3,9 @@ package com.github.apetrelli.gwtintegration.mvp.client.ui;
 import java.util.List;
 import java.util.Set;
 
-import com.google.web.bindery.requestfactory.shared.EntityProxy;
+import com.google.web.bindery.requestfactory.shared.BaseProxy;
 
-public interface ListView<T extends EntityProxy, P extends MvpView.Presenter> extends MvpView<P>{
+public interface ListView<T extends BaseProxy, P extends MvpView.Presenter> extends MvpView<P>{
 
     void setItems(List<T> items);
 
