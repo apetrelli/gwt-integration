56dd150d4e3f355e302a2feb7a63c88779825aa0
apetrelli <antonio.petrelli@gmail.com>
2014-09-01 16:53:31 +0200

    Some better groupings.

--- gwt-integration-cellview/src/main/java/com/github/apetrelli/gwtintegration/cellview/client/builder/GroupingCellTableBuilder.java
+++ gwt-integration-cellview/src/main/java/com/github/apetrelli/gwtintegration/cellview/client/builder/GroupingCellTableBuilder.java
@@ -1,6 +1,7 @@
 package com.github.apetrelli.gwtintegration.cellview.client.builder;
 
 import java.util.Collection;
+import java.util.List;
 
 import com.google.gwt.cell.client.Cell.Context;
 import com.google.gwt.cell.client.HasCell;
@@ -23,33 +24,115 @@ public class GroupingCellTableBuilder<T, I> implements CellTableBuilder<T> {
         String getClassName(T obj);
     }
 
+    public static class ColumnValue {
+        private String renderedValue;
+
+        private String className;
+
+        private int colspan;
+
+        public ColumnValue(String renderedValue, String className, int colspan) {
+            this.renderedValue = renderedValue;
+            this.className = className;
+            this.colspan = colspan;
+        }
+
+        public String getRenderedValue() {
+            return renderedValue;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        public int getColspan() {
+            return colspan;
+        }
+    }
+
+    public static class Row {
+        private List<ColumnValue> values;
+
+        private String className;
+
+        public Row(List<ColumnValue> values, String className) {
+            this.values = values;
+            this.className = className;
+        }
+
+        public List<ColumnValue> getValues() {
+            return values;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+    }
+
+    public interface ChildrenRenderer<T> {
+        List<Row> renderRows(T obj);
+    }
+
     private I id = null;
 
     private int columnCount;
 
     private GroupRenderer<T, I> renderer;
 
+    private ChildrenRenderer<T> childrenRenderer;
+
     private DefaultCellTableBuilder<T> innerBuilder;
 
     public GroupingCellTableBuilder(
             AbstractCellTable<T> cellTable, GroupRenderer<T, I> renderer) {
+        this(cellTable, renderer, null);
+    }
+
+    public GroupingCellTableBuilder(
+            AbstractCellTable<T> cellTable, GroupRenderer<T, I> renderer,
+            ChildrenRenderer<T> childrenRenderer) {
         innerBuilder = new DefaultCellTableBuilder<T>(cellTable);
         columnCount = cellTable.getColumnCount();
         this.renderer = renderer;
+        this.childrenRenderer = childrenRenderer;
     }
 
     @Override
     public void buildRow(T rowValue, int absRowIndex) {
-        I newId = renderer.getGroupId(rowValue);
-        if (!newId.equals(id)) {
-            id = newId;
-            TableRowBuilder tr = innerBuilder.startRow();
-            TableCellBuilder td = tr.startTD();
-            td.colSpan(columnCount).className(renderer.getClassName(rowValue))
-                    .text(renderer.renderGroup(rowValue)).endTD();
-            tr.endTR();
+        if (renderer != null) {
+            I newId = renderer.getGroupId(rowValue);
+            if (!newId.equals(id)) {
+                id = newId;
+                TableRowBuilder tr = innerBuilder.startRow();
+                TableCellBuilder td = tr.startTD();
+                td.colSpan(columnCount).className(renderer.getClassName(rowValue))
+                        .text(renderer.renderGroup(rowValue)).endTD();
+                tr.endTR();
+            }
         }
         innerBuilder.buildRow(rowValue, absRowIndex);
+        if (childrenRenderer != null) {
+            List<Row> rows = childrenRenderer.renderRows(rowValue);
+            if (rows != null) {
+                for (Row row : rows) {
+                    String rowClassName = row.getClassName();
+                    TableRowBuilder tr = innerBuilder.startRow();
+                    if (rowClassName != null) {
+                        tr.className(rowClassName);
+                    }
+                    for (ColumnValue value : row.getValues()) {
+                        TableCellBuilder td = tr.startTD();
+                        String columnClassName = value.getClassName();
+                        if (columnClassName != null) {
+                            td.className(columnClassName);
+                        }
+                        td.colSpan(value.getColspan()).text(value.getRenderedValue())
+                                .endTD();
+                    }
+                    tr.endTR();
+                }
+            }
+        }
     }
 
     @Override
